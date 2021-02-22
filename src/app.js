4687bfaf284423d57027a9127bf4131068aaeeaa
huangfeng <h17355450192@163.com>
2021-02-22 18:02:47 +0800

    feat: 添加登录验证接口

--- src/app.js
+++ src/app.js
@@ -1,27 +1,48 @@
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const userRouter = require('./router/user');
 const blogRouter = require('./router/blog');
 const projectRouter = require('./router/project');
+
+
+var secretkey = 'secretkey'
 const port = 3010
 
 app.use(bodyParser.json());// 添加json解析
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // 可跨域 ,编码格式
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');//的允许所有域名的端口请求（跨域解决）
-  res.header('Access-Control-Allow-Headers', 'x-requested-with, Content-Type');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Content-Type', 'application/json;charset=utf-8');
-  next();
+// app.all('*', function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');//的允许所有域名的端口请求（跨域解决）
+//   res.header('Access-Control-Allow-Headers', 'x-requested-with, Content-Type');
+//   res.header('Access-Control-Allow-Methods', '*');
+//   res.header('Content-Type', 'application/json;charset=utf-8');
+//   next();
+// })
+
+app.use(function (req, res, next) {
+  if (req.url != '/api/user/login' && req.url != '/api/user/register') {
+    let token = req.headers.authorization
+    token = token ? token.split(" ")[1] : ''
+    jwt.verify(token, secretkey, function (err, result) {
+      if (err) {
+        throw new Error('token已过期，请重新登录')
+      } else {
+        next()
+      }
+    })
+  } else {
+    next()
+  }
 })
 
+
 // 抛出接口
-app.use('/user', userRouter);
-app.use('/blog', blogRouter);
-app.use('/project', projectRouter)
+app.use('/api/user', userRouter);
+app.use('/api/blog', blogRouter);
+app.use('/api/project', projectRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- src/router/user.js
+++ src/router/user.js
@@ -1,12 +1,53 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
+const uuid = require('uuid/v1')
 
-// middleware that is specific to this router
-// router.use(function timeLog(req, res, next) {
-//   console.log(111, '11')
-//   next()
-// })
-// define the home page route
+var secretkey = 'secretkey'
+
+const router = express.Router()
+
+const connection = require('../mysql');
+
+// 注册
+router.get('/register', function (req, res) {
+  let { username, password } = req.query
+  let id = uuid().replace(/\-/g, '')
+  let md5 = crypto.createHash('md5')
+  md5.update(password)
+  password = md5.digest('hex')
+  var sql = `insert into user (id,username,password) values ('${id}','${username}','${password}')`
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log(err.message, 'err')
+    }
+    res.json({
+      code: '200',
+      message: '注册成功',
+    })
+  })
+})
+
+// 登录接口
+router.post('/login', function (req, res) {
+  let params = req.body
+  const user = {
+    username: params.username,
+    password: params.password
+  }
+  var sql = `select * from user where username = '${user.username}' and password = '${user.password}'`
+  connection.query(sql, function (err, result) {
+    if (err) throw err
+    var token = jwt.sign({ username: user.username }, secretkey, { expiresIn: 60 * 60 })
+    res.json({
+      code: '200',
+      message: '登录成功',
+      token: token
+    })
+  })
+})
+
+
 router.get('/', function (req, res) {
   res.json({
     username: '123456'
